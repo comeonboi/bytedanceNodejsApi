68fea2212615fbbca2769480e19aafc7e82ef814
hz <1322271671@qq.com>
2023-08-08 22:22:40 +0800

    修改了代码中的部分细节,让代码可读性增加了一些

--- src/authenticate/index.js
+++ src/authenticate/index.js
@@ -1,17 +1,15 @@
-const redis = require('redis');
+import redis from 'redis'
 const client = redis.createClient();
 client.on('error', (err) => console.log('Redis Client Error', err));
 client.connect();
 const authenticateToken = async (ctx, next) => {
-
   const authHeader = ctx.request.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(' ')[1]; // 根据实际情况获取令牌(传过来的token格式是Bearer xxxxx)
     const val = await client.get(token)
     if (!val) {
       ctx.status = 401; // 未授权
-      ctx.body =
-      {
+      ctx.body = {
         "success": true,
         "code": 401,
         "message": "失败",
@@ -24,7 +22,6 @@ const authenticateToken = async (ctx, next) => {
     else {
       return next()
     }
-
   } else {
     ctx.status = 401; // 未授权
     ctx.body = {
@@ -36,8 +33,6 @@ const authenticateToken = async (ctx, next) => {
         "error": "Authentication token required"
       }
     }
-
   }
-
 }
-module.exports = authenticateToken
+export default authenticateToken
